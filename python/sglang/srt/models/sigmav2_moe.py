f91b99be98529057d615ff69c01ad97c7e37446d
Zhongxin Guo <zhogu@microsoft.com>
2025-07-03 08:29:46 +0000

    make qk layernorm can be config

--- python/sglang/srt/models/sigmav2_moe.py
+++ python/sglang/srt/models/sigmav2_moe.py
@@ -124,6 +124,7 @@ class SigmaV2MoeAttention(nn.Module):
         rms_norm_eps: float = 1e-06,
         attention_bias: bool = False,
         quant_config: Optional[QuantizationConfig] = None,
+        qk_layernorm: bool = False,
         prefix: str = "",
     ) -> None:
         super().__init__()
@@ -147,6 +148,7 @@ class SigmaV2MoeAttention(nn.Module):
         self.kv_size = self.num_kv_heads * self.head_dim
         self.scaling = self.head_dim**-0.5
         self.rope_theta = rope_theta
+        self.qk_layernorm = qk_layernorm
         self.max_position_embeddings = max_position_embeddings
         self.tp_rank = get_tensor_model_parallel_rank()
 
@@ -184,17 +186,17 @@ class SigmaV2MoeAttention(nn.Module):
             prefix=add_prefix("attn", prefix),
         )
 
-        self.q_norm = RMSNorm(self.head_dim, eps=rms_norm_eps)
-        self.k_norm = RMSNorm(self.head_dim, eps=rms_norm_eps)
+        self.q_layernorm = RMSNorm(self.head_dim, eps=rms_norm_eps)
+        self.k_layernorm = RMSNorm(self.head_dim, eps=rms_norm_eps)
 
     def _apply_qk_norm(
         self, q: torch.Tensor, k: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         q_by_head = q.reshape(-1, self.head_dim)
-        q_by_head = self.q_norm(q_by_head)
+        q_by_head = self.q_layernorm(q_by_head)
         q = q_by_head.view(q.shape)
         k_by_head = k.reshape(-1, self.head_dim)
-        k_by_head = self.k_norm(k_by_head)
+        k_by_head = self.k_layernorm(k_by_head)
         k = k_by_head.view(k.shape)
         return q, k
 
@@ -206,7 +208,9 @@ class SigmaV2MoeAttention(nn.Module):
     ) -> torch.Tensor:
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
-        q, k = self._apply_qk_norm(q, k)
+        if self.qk_layernorm:
+            # Apply layer normalization to q and k
+            q, k = self._apply_qk_norm(q, k)
         q, k = self.rotary_emb(positions, q, k)
         attn_output = self.attn(q, k, v, forward_batch)
         output, _ = self.o_proj(attn_output)
