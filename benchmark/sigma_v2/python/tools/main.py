bb42075dd6dbb2b5d21866fdbd253525f57828bc
Jiamin-MS <jiaminli@microsoft.com>
2025-08-12 16:59:13 -0700

    fix lint (#34)
    Co-authored-by: root <root@RIC20PrdGPC030002Z8.ypiroefgngoulj03mx4sydf0qc.ricx.internal.cloudapp.net>

--- benchmark/sigma_v2/python/tools/main.py
+++ benchmark/sigma_v2/python/tools/main.py
@@ -1,28 +1,53 @@
 import argparse
-from model_config import ModelConfig
-from counter_registry import CounterRegistry
+
 import pandas as pd
+from counter_registry import CounterRegistry
+from model_config import ModelConfig
+
 
 def convert_to_df(models_info):
-    df = pd.DataFrame.from_dict(models_info, orient='index')
-    df.index.name = 'Model'
+    df = pd.DataFrame.from_dict(models_info, orient="index")
+    df.index.name = "Model"
     df.reset_index(inplace=True)
     return df
 
 
 def main():
     parser = argparse.ArgumentParser(description="Model Info Tool")
-    parser.add_argument("--config", type=str, default="../../model_conf/qwen_sigma/config.json", help="Path to the model configuration file")
-    parser.add_argument("--seq_len", type=int, default=1024, help="Sequence length for the model")
-    parser.add_argument("--counters", type=str, default="param", help="Comma-separated list of counters to use")
-    parser.add_argument("-s", "--save", action="store_true", help="Save the model information to a CSV file")
-    parser.add_argument("-o", "--output", type=str, default="model_info.csv", help="Output file name for saving model information")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="../../model_conf/qwen_sigma/config.json",
+        help="Path to the model configuration file",
+    )
+    parser.add_argument(
+        "--seq_len", type=int, default=1024, help="Sequence length for the model"
+    )
+    parser.add_argument(
+        "--counters",
+        type=str,
+        default="param",
+        help="Comma-separated list of counters to use",
+    )
+    parser.add_argument(
+        "-s",
+        "--save",
+        action="store_true",
+        help="Save the model information to a CSV file",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="model_info.csv",
+        help="Output file name for saving model information",
+    )
     args = parser.parse_args()
-    
+
     seq_len = args.seq_len
-    counter_names = args.counters.split(',')
+    counter_names = args.counters.split(",")
     models_info = {}
-    for config_path in args.config.split(','):
+    for config_path in args.config.split(","):
         print(f"Processing config: {config_path}")
         config = ModelConfig.from_json_file(config_path)
         models_info[config.name] = {}
@@ -40,6 +65,7 @@ def main():
     if args.save:
         print(f"Saving model information to: {args.output}")
         df.to_csv(args.output, index=False)
+
 
 if __name__ == "__main__":
     main()
