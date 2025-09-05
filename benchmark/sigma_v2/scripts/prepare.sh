0696c84cf65f43440ae45cb3403026691bb02450
root <root@RIC20PrdGPC030002Z6.ypiroefgngoulj03mx4sydf0qc.ricx.internal.cloudapp.net>
2025-09-05 20:54:32 +0000

    remove redundant files

--- benchmark/sigma_v2/scripts/prepare.sh
+++
@@ -1,8 +0,0 @@
-cd model_conf
-cd deepseek
-wget https://huggingface.co/deepseek-ai/DeepSeek-V3/resolve/main/tokenizer.json
-wget https://huggingface.co/deepseek-ai/DeepSeek-V3/resolve/main/configuration_deepseek.py
-wget https://huggingface.co/deepseek-ai/DeepSeek-V3/resolve/main/tokenizer_config.json
-cd ../qwen_sigma
-wget https://huggingface.co/Qwen/Qwen3-235B-A22B/resolve/main/tokenizer.json
-wget https://huggingface.co/Qwen/Qwen3-235B-A22B/resolve/main/tokenizer_config.json
