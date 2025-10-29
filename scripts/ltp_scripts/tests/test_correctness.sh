1ef72a8ece54343f9feaf31bc1ebdc90af9c56f9
Yifan Xiong <yifan.xiong@microsoft.com>
2025-10-28 17:15:15 -0700

    Update
    Update.

--- scripts/ltp_scripts/tests/test_correctness.sh
+++ scripts/ltp_scripts/tests/test_correctness.sh
@@ -6,9 +6,14 @@
 
 export TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-/mnt/output}"
 
-modules=("attention" "moe" "logits" "rmsnorm" "rope" "token_embedding")
+modules=("attention" "logits" "rmsnorm" "rope" "token_embedding")
 for module in "${modules[@]}"; do
-    echo "=== Test correctness for $fruit ==="
+    echo "*** Test correctness for $module ***"
     pytest -s "$(dirname $0)/../../../test/srt/numerical_tests/comparison_tests/benchmark/test_bench_$module.py"
-    TEST_BENCHMARK_DIR="$TEST_OUTPUT_DIR/bench/$module" pytest -s "$(dirname $0)/../../../test/srt/numerical_tests/comparison_tests/test_${module_name}_comparison.py"
+    TEST_BENCHMARK_DIR="$TEST_OUTPUT_DIR/bench/$module" pytest -s "$(dirname $0)/../../../test/srt/numerical_tests/comparison_tests/test_${module}_comparison.py"
+done
+
+for f in /mnt/output/*-compare/*/*.json; do
+    printf '\n*** %s ***\n' "$f"
+    jq . "$f"
 done
